c2ef41ef1f993d28fb0f564d9b6015e2783900e7
Langning Chen <chenlangning2009@163.com>
2024-03-08 21:48:58 +0800

    Add color settings

--- main.cpp
+++ main.cpp
@@ -41,8 +41,10 @@ _tstring ReplaceAll(_tstring Input, _tstring Find, _tstring Replace)
     return Input;
 }
 
-_tstring CountdownString;
+_tstring CountdownString, FontName;
 int FontHeight;
+int BackgroundColorRed, BackgroundColorGreen, BackgroundColorBlue;
+int ForegroundColorRed, ForegroundColorGreen, ForegroundColorBlue;
 
 int WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
 {
@@ -51,26 +53,48 @@ int WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int n
     _tstring CurrentFilePath = CurrentFilePathBuffer;
     CurrentFilePath = CurrentFilePath.substr(0, CurrentFilePath.find_last_of('\\') + 1);
     _tstring INIPath = CurrentFilePath + TEXT("Settings.ini");
-    TCHAR CountdownStringBuffer[MAX_PATH] = {0};
-    GetPrivateProfileString(TEXT("Settings"), TEXT("CountdownString"), TEXT(""), CountdownStringBuffer, MAX_PATH, INIPath.c_str());
-    CountdownString = CountdownStringBuffer;
-    int TargetYear = GetPrivateProfileInt(TEXT("Settings"), TEXT("TargetYear"), -1, INIPath.c_str());
-    int TargetMonth = GetPrivateProfileInt(TEXT("Settings"), TEXT("TargetMonth"), -1, INIPath.c_str());
-    int TargetDay = GetPrivateProfileInt(TEXT("Settings"), TEXT("TargetDay"), -1, INIPath.c_str());
-    FontHeight = GetPrivateProfileInt(TEXT("Settings"), TEXT("FontHeight"), -1, INIPath.c_str());
 
-    if (CountdownString == TEXT("") || TargetYear == -1 || TargetMonth == -1 || TargetDay == -1 || FontHeight == -1)
+    TCHAR CountdownStringBuffer[MAX_PATH] = {0};
+    GetPrivateProfileString(TEXT("Display"), TEXT("CountdownString"), TEXT(""), CountdownStringBuffer, MAX_PATH, INIPath.c_str());
+    CountdownString = CountdownStringBuffer;
+
+    TCHAR FontNameBuffer[MAX_PATH] = {0};
+    GetPrivateProfileString(TEXT("Display"), TEXT("FontName"), TEXT(""), FontNameBuffer, MAX_PATH, INIPath.c_str());
+    FontName = FontNameBuffer;
+
+    int TargetYear = GetPrivateProfileInt(TEXT("Target"), TEXT("Year"), -1, INIPath.c_str());
+    int TargetMonth = GetPrivateProfileInt(TEXT("Target"), TEXT("Month"), -1, INIPath.c_str());
+    int TargetDay = GetPrivateProfileInt(TEXT("Target"), TEXT("Day"), -1, INIPath.c_str());
+    FontHeight = GetPrivateProfileInt(TEXT("Display"), TEXT("FontHeight"), -1, INIPath.c_str());
+
+    BackgroundColorRed = GetPrivateProfileInt(TEXT("BackgroundColor"), TEXT("Red"), -1, INIPath.c_str());
+    BackgroundColorGreen = GetPrivateProfileInt(TEXT("BackgroundColor"), TEXT("Green"), -1, INIPath.c_str());
+    BackgroundColorBlue = GetPrivateProfileInt(TEXT("BackgroundColor"), TEXT("Blue"), -1, INIPath.c_str());
+
+    ForegroundColorRed = GetPrivateProfileInt(TEXT("ForegroundColor"), TEXT("Red"), -1, INIPath.c_str());
+    ForegroundColorGreen = GetPrivateProfileInt(TEXT("ForegroundColor"), TEXT("Green"), -1, INIPath.c_str());
+    ForegroundColorBlue = GetPrivateProfileInt(TEXT("ForegroundColor"), TEXT("Blue"), -1, INIPath.c_str());
+
+    if (CountdownString == TEXT("") || FontName == TEXT("") || TargetYear == -1 || TargetMonth == -1 || TargetDay == -1 || FontHeight == -1 || ForegroundColorRed == -1 || ForegroundColorGreen == -1 || ForegroundColorBlue == -1)
     {
         CountdownString = TEXT("${AllDaysLeft} days till 01/01/2035");
+        FontName = TEXT("Arial");
         TargetYear = 2035;
         TargetMonth = 1;
         TargetDay = 1;
         FontHeight = 50;
-        WritePrivateProfileString(TEXT("Settings"), TEXT("CountdownString"), CountdownString.c_str(), INIPath.c_str());
-        WritePrivateProfileString(TEXT("Settings"), TEXT("TargetYear"), _ttostring(TargetYear).c_str(), INIPath.c_str());
-        WritePrivateProfileString(TEXT("Settings"), TEXT("TargetMonth"), _ttostring(TargetMonth).c_str(), INIPath.c_str());
-        WritePrivateProfileString(TEXT("Settings"), TEXT("TargetDay"), _ttostring(TargetDay).c_str(), INIPath.c_str());
-        WritePrivateProfileString(TEXT("Settings"), TEXT("FontHeight"), _ttostring(FontHeight).c_str(), INIPath.c_str());
+        ForegroundColorRed = 255;
+        ForegroundColorGreen = 0;
+        ForegroundColorBlue = 0;
+        WritePrivateProfileString(TEXT("Display"), TEXT("CountdownString"), CountdownString.c_str(), INIPath.c_str());
+        WritePrivateProfileString(TEXT("Display"), TEXT("FontName"), FontName.c_str(), INIPath.c_str());
+        WritePrivateProfileString(TEXT("Target"), TEXT("Year"), _ttostring(TargetYear).c_str(), INIPath.c_str());
+        WritePrivateProfileString(TEXT("Target"), TEXT("Month"), _ttostring(TargetMonth).c_str(), INIPath.c_str());
+        WritePrivateProfileString(TEXT("Target"), TEXT("Day"), _ttostring(TargetDay).c_str(), INIPath.c_str());
+        WritePrivateProfileString(TEXT("Display"), TEXT("FontHeight"), _ttostring(FontHeight).c_str(), INIPath.c_str());
+        WritePrivateProfileString(TEXT("ForegroundColor"), TEXT("Red"), _ttostring(ForegroundColorRed).c_str(), INIPath.c_str());
+        WritePrivateProfileString(TEXT("ForegroundColor"), TEXT("Green"), _ttostring(ForegroundColorGreen).c_str(), INIPath.c_str());
+        WritePrivateProfileString(TEXT("ForegroundColor"), TEXT("Blue"), _ttostring(ForegroundColorBlue).c_str(), INIPath.c_str());
     }
 
     SYSTEMTIME TargetDate = {0};
@@ -108,11 +132,14 @@ int WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int n
         {
         case WM_CREATE:
         {
-            HMODULE hModule = LoadLibrary(TEXT("user32.dll"));
-            pSetWindowCompositionAttribute SetWindowCompositionAttribute = (pSetWindowCompositionAttribute)GetProcAddress(hModule, "SetWindowCompositionAttribute");
-            AccentPolicy Policy = {4, 0, 0x00FFFFFF, 0};
-            WindowCompositionAttributeData Data = {19, &Policy, sizeof(AccentPolicy)};
-            SetWindowCompositionAttribute(hWnd, &Data);
+            if (BackgroundColorRed == -1 || BackgroundColorGreen == -1 || BackgroundColorBlue == -1)
+            {
+                HMODULE hModule = LoadLibrary(TEXT("user32.dll"));
+                pSetWindowCompositionAttribute SetWindowCompositionAttribute = (pSetWindowCompositionAttribute)GetProcAddress(hModule, "SetWindowCompositionAttribute");
+                AccentPolicy Policy = {4, 0, 0x00FFFFFF, 0};
+                WindowCompositionAttributeData Data = {19, &Policy, sizeof(AccentPolicy)};
+                SetWindowCompositionAttribute(hWnd, &Data);
+            }
             break;
         }
         case WM_PAINT:
@@ -122,12 +149,15 @@ int WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int n
             HDC Hdc = BeginPaint(hWnd, &Paint);
             RECT Rect;
             GetClientRect(hWnd, &Rect);
-            SetBkMode(Hdc, TRANSPARENT);
-            SetTextColor(Hdc, RGB(255, 0, 0));
+            if (BackgroundColorRed != -1 && BackgroundColorGreen != -1 && BackgroundColorBlue != -1)
+                SetBkColor(Hdc, RGB(BackgroundColorRed, BackgroundColorGreen, BackgroundColorBlue));
+            else
+                SetBkMode(Hdc, TRANSPARENT);
+            SetTextColor(Hdc, RGB(ForegroundColorRed, ForegroundColorGreen, ForegroundColorBlue));
             LOGFONT Font = {0};
             Font.lfHeight = FontHeight;
             Font.lfWeight = FW_NORMAL;
-            _tcscpy(Font.lfFaceName, TEXT("楷体"));
+            _tcscpy(Font.lfFaceName, FontName.c_str());
             SelectObject(Hdc, CreateFontIndirect(&Font));
             DrawText(Hdc, CountdownString.c_str(), CountdownString.length(), &Rect, DT_CENTER | DT_VCENTER);
             EndPaint(hWnd, &Paint);
